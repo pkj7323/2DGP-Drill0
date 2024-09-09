9584663ed9e410f9788780d83cf819911ad4b034
leopark <pkj7323@naver.com>
2024-09-09 14:36:22 +0900

    터틀 육각형

---
+++ sourceCode/turtlePrac.py
@@ -0,0 +1,15 @@
+import turtle
+from time import sleep
+
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+sleep(1000)
