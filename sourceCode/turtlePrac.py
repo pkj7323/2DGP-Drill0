b5cda4d15f02358a170bfe0f20a282cb9c202ab5
leopark <pkj7323@naver.com>
2024-09-09 14:38:30 +0900

    터틀 원 그리기

--- sourceCode/turtlePrac.py
+++ sourceCode/turtlePrac.py
@@ -1,15 +1,15 @@
 import turtle
 from time import sleep
 
-turtle.forward(100)
-turtle.left(60)
-turtle.forward(100)
-turtle.left(60)
-turtle.forward(100)
-turtle.left(60)
-turtle.forward(100)
-turtle.left(60)
-turtle.forward(100)
-turtle.left(60)
-turtle.forward(100)
+turtle.circle(100)
+turtle.right(90)
+turtle.circle(10)
+turtle.right(90)
+turtle.circle(40)
+turtle.right(90)
+turtle.circle(80)
+turtle.right(90)
+turtle.undo()
+turtle.undo()
+
 sleep(1000)
